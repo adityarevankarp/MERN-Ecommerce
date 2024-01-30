41b1aca8ba240cce7fa88d4358431b8345bbad38
Aditya Revankar <adityarevankar03@gmail.com>
2024-01-31 01:38:21 +0530

    Added homepage frontend code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
 import Navbar from "./Components/Navbar/Navbar"
 
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Shop from "./Pages/Shop"
+import ShopCategory from "./Pages/ShopCategory"
+import Product from "./Pages/Product"
+import LoginSignup from "./Pages/LoginSignup"
+import Cart from "./Pages/Cart"
 function App() {
   
 
   return (
     <div>
-      <Navbar></Navbar>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop/>}/>
+        <Route path="/mens" element={<ShopCategory category = 'mens'/>}/>
+        <Route path="/womens" element={<ShopCategory category = 'women'/>}/>
+        <Route path="/kids" element={<ShopCategory category = 'kids'/>}/>
+        <Route path="/product" element={<Product/>}/>
+        <Route path=':productId' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   )
 }
